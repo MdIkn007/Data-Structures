1f6a69448b9c3379a2ec1c321a392aae33b57f82
MdIkn007 <160925733096@lords.ac.in>
2026-09-09 12:35:01 +0530

    Add files via upload

---
+++ 027_electricity_bill.py
@@ -0,0 +1,9 @@
+units = int(input())
+bill = 0
+if units <= 100:
+    bill = units * 1.5
+elif units <= 200:
+    bill = (100 * 1.5) + (units - 100) * 2.5
+else:
+    bill = (100 * 1.5) + (100 * 2.5) + (units - 200) * 4.0
+print(f"Bill: {bill}")
